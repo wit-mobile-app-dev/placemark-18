8834753e4d3254c883924db011250ca6dab9ff66
Eamonn de Leastar <edeleastar@wit.ie>
2018-11-03 17:54:55 +0000

    refactor models packages

--- app/src/main/java/org/wit/placemark/main/MainApp.kt
+++ app/src/main/java/org/wit/placemark/main/MainApp.kt
@@ -3,8 +3,6 @@ package org.wit.placemark.main
 import android.app.Application
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
-import org.wit.placemark.models.PlacemarkJSONStore
-import org.wit.placemark.models.PlacemarkMemStore
 import org.wit.placemark.models.PlacemarkStore
 import org.wit.placemark.room.PlacemarkStoreRoom
 
